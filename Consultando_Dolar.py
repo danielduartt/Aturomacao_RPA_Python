2d79ecc2b80f120b0f0b7c232e2bc42b8fbddf4f
Daniel Duarte <dev.daniel.duarte@gmail.com>
2023-08-05 11:22:43 -0300

    Add projeto simples: Consultando Dolar no brave

---
+++ Consultando_Dolar.py
@@ -0,0 +1,16 @@
+import pyautogui as pag
+pag.sleep(1)
+
+
+pag.moveTo(x=908, y=1054)
+pag.sleep(2)
+pag.click(x=908, y=1054)
+pag.press('enter')
+pag.sleep(2)
+pag.moveTo(x=975, y=44)
+pag.click(x=975, y=44)
+pag.sleep(1)
+pag.press('del')
+pag.typewrite("Valor dolar")
+pag.sleep(1)
+pag.press('enter')
